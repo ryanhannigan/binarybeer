990b1ea50f29b3822d1e82eb76ec23e40f05b8f3
Joe Purnell <joepurnell1@gmail.com>
2019-08-29 18:45:15 +0100

    add pub list data fetch

--- src/pages/pub-list.js
+++ src/pages/pub-list.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlueContainer from '../components/bluecontainer';
 import Pub from '../components/publisting';
 import Title from '../components/Title';
 
 export default () => {
+  const [ pubList, setPubList ] = useState([]);
+
+  useEffect(() => {
+    //fetch pub list
+
+    setPubList([
+      {
+        imageSrc: 'http://www.thoroughlymodernmilly.com/wp-content/uploads/2012/04/IMG_6262.jpg',
+        pubname: 'The Mall',
+        rating: 4,
+        pubId: 'themall',
+      },
+      {
+        imageSrc: 'http://www.thoroughlymodernmilly.com/wp-content/uploads/2012/04/IMG_6262.jpg',
+        pubname: 'The George',
+        rating: 2,
+        pubId: 'thegeorge',
+      },
+      {
+        imageSrc: 'http://www.thoroughlymodernmilly.com/wp-content/uploads/2012/04/IMG_6262.jpg',
+        pubname: 'The Stepping Stone',
+        rating: 5,
+        pubId: 'stepping',
+      },
+    ])
+  }, [])
+
   return (
     <BlueContainer>
       <Title>Pub List</Title>
-
-      <Pub imageSrc="http://www.thoroughlymodernmilly.com/wp-content/uploads/2012/04/IMG_6262.jpg" pubname="The Mall" rating={4} pubId="themall"/>
-      <Pub imageSrc="http://www.thoroughlymodernmilly.com/wp-content/uploads/2012/04/IMG_6262.jpg" pubname="The George" rating={4} pubId="thegeorge"/>
-      <Pub imageSrc="http://www.thoroughlymodernmilly.com/wp-content/uploads/2012/04/IMG_6262.jpg" pubname="Spoons" rating={4} pubId="spoons"/>
+      {
+        pubList && pubList.map((pub) => (
+          <Pub imageSrc={pub.imageSrc} pubname={pub.pubname} rating={pub.rating} pubId={pub.pubId} />
+        ))
+      }
     </BlueContainer>
   );
 }
